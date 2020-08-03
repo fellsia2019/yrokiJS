e5f7ca83e1d50aa16b5198969a3ef9340dfda744
Artem <mushket.1998@mail.ru>
2020-08-03 15:08:06 +0300

    2.10

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let money = prompt("Ваш бюджет на месяц", "");
+let money = +prompt("Ваш бюджет на месяц", "");
 let time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 
@@ -29,7 +29,59 @@ for (let i = 0; i < 2; i++ ) {
         console.log('done');
         appData.expenses[a] = b;
     } else {
-
+        console.log ("bad result");
+        i--;
     }
+};
+
+appData.moneyPerDay = appData.budget/30;
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+    console.log("Мин ур достатка")
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Сред ур достатка");
+} else if (appData.moneyPerDay > 2000){
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Упс, ошибка")
 }
-alert(appData.budget/30);
+
+
+// цикл while
+// let i = 0;
+// while(i < 2){
+//     let a = prompt("Введите обязательную статью расходов в этом месяце") ;
+//     let b = prompt("Во сколько обойдется?") ;
+//     if ( (typeof(a) ) === 'string' && (typeof(a)) != null && (typeof(b)) != null 
+//         && a!= '' && b != '' && a.length <50) {
+//         console.log('done');
+//         appData.expenses[a] = b;
+//     } else {
+//         console.log ("bad result");
+//         i--;
+//     }
+//     i++;
+// }
+
+// // Используем цикл DO...WHILE
+
+// let i = 0;
+// do {
+//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt ("Во сколько обойдется?", "");
+
+//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+//         console.log ("done");
+
+//         appData.expenses[a] = b;
+//     } else {
+//          console.log ("bad result");
+//          i--;
+//     }
+
+//     i++;
+// }
+// while(i < 2);
