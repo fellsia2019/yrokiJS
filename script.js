d95461f18b9e5c74d36e18c9e11808d768810292
Artem <mushket.1998@mail.ru>
2020-08-07 16:38:15 +0300

    3st commit

--- script.js
+++ script.js
@@ -15,47 +15,40 @@ function start() {
 start();
 
 
-
-
-
-
-let optionalExpenses ;
+// let optionalExpenses ;
 
 let appData = {
-    budget : money,
-    timeData : time,
-    expenses : {},
-    optionalExpenses : {},
-    income :[],
-    savings :true
-
-}
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: true
+};
 
 
 function chooseExpenses() {
-    for (let i = 0; i < 2; i++ ) {
-        let a = prompt("Введите обязательную статью расходов в этом месяце") ;
-        let b = prompt("Во сколько обойдется?") ;
-        
-        if ( (typeof(a) ) === 'string' && (typeof(a)) != null && (typeof(b)) != null 
-            && a!= '' && b != '' && a.length <50) {
-            console.log('done');
+    for (let i = 0; i < 2; i++) {
+        let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+            b = prompt ("Во сколько обойдется?", "");
+    
+        if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
             appData.expenses[a] = b;
         } else {
-            console.log ("bad result");
             i--;
         }
-    }
+    
+    };
 }
 chooseExpenses();
 
-function detectDayBudget() {
+function detectDayBudget() {                                            // Расчет дневного бюджета
     appData.moneyPerDay = (appData.budget / 30).toFixed();
-    alert("Бюджет на 1 день состовляет" + appData.moneyPerDay = "руб.")ж
+    alert ("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
 }
 detectDayBudget();
 
-function detectLevel( {
+function detectLevel() {
     if(appData.moneyPerDay < 100) {
         console.log("Мин ур достатка")
     } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
@@ -65,7 +58,7 @@ function detectLevel( {
     } else {
         console.log("Упс, ошибка")
     }
-})
+};
 detectLevel();
 
 // alert("Ежедневный бюджет: " + appData.moneyPerDay);
